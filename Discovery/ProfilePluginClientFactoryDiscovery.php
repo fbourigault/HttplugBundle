4a6c8250998acbfd145d519f319100d436a9830c
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-05-26 21:08:06 +0200

    Remove need for discovery

--- Discovery/ProfilePluginClientFactoryDiscovery.php
+++ Discovery/ProfilePluginClientFactoryDiscovery.php
@@ -1,16 +1,13 @@
 <?php
 
-
 namespace Http\HttplugBundle\Discovery;
 
+use Http\Client\Common\PluginClientFactory;
 use Http\Client\Common\PluginClientFactoryInterface;
-use Http\Discovery\Exception\StrategyUnavailableException;
-use Http\Discovery\PluginClientFactoryDiscovery;
-use Http\Discovery\Strategy\DiscoveryStrategy;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-final class ProfilePluginClientFactoryDiscovery implements DiscoveryStrategy, EventSubscriberInterface
+final class ProfilePluginClientFactoryDiscovery implements EventSubscriberInterface
 {
     /**
      * @var PluginClientFactoryInterface
@@ -26,34 +23,13 @@ final class ProfilePluginClientFactoryDiscovery implements DiscoveryStrategy, Ev
     }
 
     /**
-     * Find a resource of a specific type.
-     *
-     * @param string $type
-     *
-     * @return array The return value is always an array with zero or more elements. Each
-     *               element is an array with two keys ['class' => string, 'condition' => mixed].
-     *
-     * @throws StrategyUnavailableException if we cannot use this strategy.
-     */
-    public static function getCandidates($type)
-    {
-        if ($type === PluginClientFactoryInterface::class && self::$factory !== null) {
-            return [['class' => function () {
-                return self::$factory;
-            }]];
-        }
-
-        return [];
-    }
-
-    /**
      * Make sure to use our custom strategy.
      *
      * @param Event $e
      */
     public function onEvent(Event $e)
     {
-        PluginClientFactoryDiscovery::prependStrategy(self::class);
+        PluginClientFactory::setFactory(self::$factory);
     }
 
     /**
